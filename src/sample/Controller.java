b6c99ddb406c4ea7991946f755d4f014b28908ac
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-11-24 16:16:58 +0100

    Spinner hozzáadva

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,19 +1,20 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class Controller {
 
     public Label welcomeText;
     public TextField textField;
+    public Label eletkorLabel;
+    public Spinner<Integer> eletkor;
 
-    public void helloClick(ActionEvent actionEvent) {
+    public void felvetelClick(ActionEvent actionEvent) {
+        Alert alert = new Alert(Alert.AlertType.NONE);
+        alert.setContentText("Szia " + textField.getText());
+        alert.getButtonTypes().add(ButtonType.OK);
+        alert.showAndWait();
         welcomeText.setText("Szia "+ textField.getText());
-        Alert alert = new Alert(Alert.AlertType.NONE, "Hello " + textField.getText(), ButtonType.OK);
-        alert.show();
-    }
 }
+    }
