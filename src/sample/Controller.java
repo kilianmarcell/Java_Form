d8c1a22fd805c54bbb1665c9d04058587525eced
kilianmarcell <kilian.marcell.andras@gmail.com>
2021-11-24 15:35:45 +0100

    Bezárható felugró ablak

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,13 +1,19 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
     public Label welcomeText;
+    public TextField textField;
 
     public void helloClick(ActionEvent actionEvent) {
-        welcomeText.setText("Csok!");
+        welcomeText.setText("Szia "+ textField.getText());
+        Alert alert = new Alert(Alert.AlertType.NONE, "Hello " + textField.getText(), ButtonType.OK);
+        alert.show();
     }
 }
